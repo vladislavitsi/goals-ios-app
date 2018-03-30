8883e2283b88ef7d0738f74551d98c712b4349eb
Vladislavitsi <vladislavitsi@gmail.com>
2018-03-30 10:58:00 +0300

    Initial Commit

---
+++ Number/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Number
+//
+//  Created by Владислав Клещенко on 3/30/18.
+//  Copyright © 2018 vladislavitsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Number/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Number
+//
+//  Created by Владислав Клещенко on 3/30/18.
+//  Copyright © 2018 vladislavitsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
